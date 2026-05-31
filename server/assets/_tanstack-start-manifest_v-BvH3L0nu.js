9a965076470540559c978893e193242032058c1a
Samadhi Wijekoon <samadhiwijekoon51@gmail.com>
2026-05-31 23:51:05 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-BvH3L0nu.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Samadhi/Downloads/portfolio-project/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BT0sUZl-.js"] }, "/": { filePath: "C:/Users/Samadhi/Downloads/portfolio-project/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-HHWpHp1o.js"] } }, clientEntry: "/assets/index-BT0sUZl-.js" });
-export {
-  tsrStartManifest
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-_CTM9MNq.js";
-import "node:events";
-export {
-  w as default
-};
